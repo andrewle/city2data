e89ccb8e74fe49502c8003c57850475950a0ca7b
Andrew Le <andrew@andrewdle.com>
2011-02-19 16:33:56 -0800

    Add integration specs

--- lib/city2data.rb
+++ lib/city2data.rb
@@ -1,4 +1,8 @@
 require 'sinatra/base'
+require 'twitter'
+
+class Dispatch
+end
 
 class City2Data < Sinatra::Base
 
@@ -6,5 +10,8 @@ class City2Data < Sinatra::Base
     200
   end
 
-
+  post '/update' do
+    last_dispatch = Dispatch.last
+    Twitter.user_timeline('SBCFireDispatch', status_id: last_dispatch[:status_id])
+  end
 end
--- spec/integration/city2data_spec.rb
+++ spec/integration/city2data_spec.rb
@@ -1,4 +1,5 @@
 require 'city2data'
+require 'rspec'
 require 'rack/test'
 
 describe "City2Data application" do
@@ -11,5 +12,29 @@ describe "City2Data application" do
   it "should respond to '/'" do
     get '/'
     last_response.should be_ok
+  end
+
+  describe "POST /update" do
+    
+    before(:each) do
+      Dispatch.stub(:last).and_return(status_id: 12345)
+      Twitter.stub(:user_timeline)
+    end
+
+    it "should respond ok" do
+      post '/update'
+      last_response.should be_ok
+    end
+
+    it "should find the last dispatch tweet" do
+      Dispatch.should_receive(:last)
+      post '/update'
+    end
+
+    it "should query Twitter for SBCFireDispatch's timeline since the last tweet" do
+      Twitter.should_receive(:user_timeline)
+             .with('SBCFireDispatch', status_id: 12345)
+      post '/update'
+    end
   end
 end
