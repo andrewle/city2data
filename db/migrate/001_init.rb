f4e472df0dbda91b5e7de9c50a043a9f58cb5811
Andrew Le <andrew@andrewdle.com>
2011-02-19 17:12:41 -0800

    Add initial migration

---
+++ db/migrate/001_init.rb
@@ -0,0 +1,21 @@
+class Init < ActiveRecord::Migration
+  def self.up
+    create_table :dispatches do |t|
+      t.integer :status_id
+      t.string  :address
+      t.string  :city
+      t.string  :zip_code
+      t.string  :type
+      t.string  :incident_num_one
+      t.string  :incident_num_two
+      t.string  :latitude
+      t.string  :longitude
+      t.text    :json_data
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :dispatches
+  end
+end
