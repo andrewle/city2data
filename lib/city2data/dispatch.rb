222457fae25a49e2719d918ed2bdc73b9bfb8efd
Andrew Le <andrew@andrewdle.com>
2011-02-27 05:22:15 -0800

    Shuffled the dispatch code around a bit

--- lib/city2data/dispatch.rb
+++ lib/city2data/dispatch.rb
@@ -5,19 +5,19 @@ class Dispatch < ActiveRecord::Base
 
   TWITTER_USERNAME = 'SBCFireDispatch'
 
-  def self.new_from_tweet(tweet)
-    new(Tweet.new(tweet).parse)
+  def self.updates!
+    options = { since_id: last_status_id }
+    Twitter.user_timeline(TWITTER_USERNAME, options).each do |tweet|
+      new_from_tweet(tweet).save!
+    end
   end
 
   def self.last_status_id
     order('status_id DESC').first.status_id
   end
 
-  def self.updates!
-    options = { since_id: last_status_id }
-    Twitter.user_timeline(TWITTER_USERNAME, options).each do |tweet|
-      new_from_tweet(tweet).save!
-    end
+  def self.new_from_tweet(tweet)
+    new(Tweet.new(tweet).parse)
   end
 
   def geocode
