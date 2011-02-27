494c2e96b82b7fe5f49bb9a8659e97238148ca54
Andrew Le <andrew@andrewdle.com>
2011-02-27 05:22:53 -0800

    Update spec_helper, use test ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
 require 'rubygems'
 require 'spork'
 
+ENV['RACK_ENV'] = 'test'
+
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  require File.join(File.dirname(__FILE__), '..', 'lib', 'city2data', 'init')
   require 'rspec'
   require 'rack/test'
 
