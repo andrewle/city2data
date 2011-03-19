4391b9a084aee7d28523c7345ae3e518577a8959
Andrew Le <andrew@andrewdle.com>
2011-03-19 15:46:45 -0700

    Add sample bar chart and gRaphael

--- public/graphs/js/site.js
+++ public/graphs/js/site.js
@@ -1,7 +1,21 @@
+window.Data = [55, 20, 13, 32, 5, 1, 2, 10];
+
 $(function () {
 
   $('#header .nav li').click(function () {
     $(this).siblings().removeClass('current');
     $(this).addClass('current');
   });
+
+  var r = Raphael("holder");
+  var data = window.Data;
+  var chart = r.g.barchart(0, 10, 700, 345, data);
+
+  var labels = [];
+  $.each(data, function (i, val) {
+    labels.push('label'  + i);
+  });
+
+  chart.label(labels, true);
+  console.log(labels);
 });
