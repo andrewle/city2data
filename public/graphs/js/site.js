e62f633cf472a6ce637e353866a326fe0bfb1b4a
Andrew Le <andrew@andrewdle.com>
2011-03-19 16:13:36 -0700

    Refactored charting code

--- public/graphs/js/site.js
+++ public/graphs/js/site.js
@@ -1,21 +1,37 @@
-window.Data = [55, 20, 13, 32, 5, 1, 2, 10];
+var Chart = function () {
+  this.r = Raphael("the-graph");
+  this.fetchData();
+  this.draw();
+  return this.chart;
+};
+
+Chart.prototype.fetchData = function () {
+  var data = {
+    values: [55, 20, 13, 32, 5, 1, 2, 10],
+    labels: []
+  };
+
+  $.each(data.values, function (i, val) {
+    data.labels.push('label'  + i);
+  });
+
+  return (this.data = data);
+};
+
+Chart.prototype.draw = function () {
+  var 
+      data   = this.data.values,
+      labels = this.data.labels,
+      chart  = this.r.g.barchart(0, 10, 700, 345, data);
+  chart.label(labels, true);
+  return (this.chart = chart);
+};
 
 $(function () {
-
   $('#header .nav li').click(function () {
     $(this).siblings().removeClass('current');
     $(this).addClass('current');
   });
 
-  var r = Raphael("holder");
-  var data = window.Data;
-  var chart = r.g.barchart(0, 10, 700, 345, data);
-
-  var labels = [];
-  $.each(data, function (i, val) {
-    labels.push('label'  + i);
-  });
-
-  chart.label(labels, true);
-  console.log(labels);
+  var chart = new Chart();
 });
