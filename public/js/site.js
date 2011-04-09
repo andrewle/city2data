aebaa57e9891c00cab0c0663b3902c7a522081d1
Andrew Le <andrew@andrewdle.com>
2011-04-09 12:26:30 -0700

    Moved color data out

--- public/js/site.js
+++ public/js/site.js
@@ -26,39 +26,36 @@ Chart.prototype.fetchData = function () {
 Chart.processDataAndDraw = function () {
 };
 
-Chart.prototype.generateColors = function () {
-  var colors = [
-    "rgb(247, 150, 103)",
-    "rgb(244, 122, 75)",
-    "rgb(242, 93, 69)",
-    "rgb(210, 31, 47)",
-    "rgb(209, 28, 89)",
-    "rgb(237, 0, 136)",
-    "rgb(131, 56, 133)",
-    "rgb(126, 102, 157)",
-    "rgb(128, 160, 207)",
-    "rgb(91, 179, 223)",
-    "rgb(0, 175, 233)",
-    "rgb(0, 156, 211)",
-    "rgb(1, 113, 153)",
-    "rgb(0, 173, 176)",
-    "rgb(52, 144, 123)",
-    "rgb(50, 143, 72)",
-    "rgb(73, 107, 44)",
-    "rgb(115, 118, 40)",
-    "rgb(187, 165, 53)",
-    "rgb(255, 216, 124)",
-    "rgb(243, 235, 8)",
-    "rgb(253, 192, 131)"
-  ];
-  return colors;
-};
+Chart.colors = [
+  "rgb(247, 150, 103)",
+  "rgb(244, 122, 75)",
+  "rgb(242, 93, 69)",
+  "rgb(210, 31, 47)",
+  "rgb(209, 28, 89)",
+  "rgb(237, 0, 136)",
+  "rgb(131, 56, 133)",
+  "rgb(126, 102, 157)",
+  "rgb(128, 160, 207)",
+  "rgb(91, 179, 223)",
+  "rgb(0, 175, 233)",
+  "rgb(0, 156, 211)",
+  "rgb(1, 113, 153)",
+  "rgb(0, 173, 176)",
+  "rgb(52, 144, 123)",
+  "rgb(50, 143, 72)",
+  "rgb(73, 107, 44)",
+  "rgb(115, 118, 40)",
+  "rgb(187, 165, 53)",
+  "rgb(255, 216, 124)",
+  "rgb(243, 235, 8)",
+  "rgb(253, 192, 131)"
+];
 
 Chart.prototype.draw = function () {
   var 
       data   = this.data.values,
       labels = this.data.labels,
-      opts   = {colors: this.generateColors() },
+      opts   = {colors: Chart.colors },
       chart  = this.r.g.barchart(0, 10, 700, 345, data, opts);
   return (this.chart = chart);
 };
